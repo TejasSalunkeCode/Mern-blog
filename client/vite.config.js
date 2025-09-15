a876de755eba77978498d3ae4432b13b76f452bc
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-09-15 17:34:55 +0530

    create the header section

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,35 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react-swc'
 
-// // https://vite.dev/config/
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
+
+// import { defineConfig } from 'vite'
+
+
 // export default defineConfig({
-//   plugins: [react()],
+//   // server: {
+//   //   proxy: {
+//   //     '/api': {
+//   //       target: 'http://localhost:3000',
+//   //       secure: false,
+//   //     },
+//   //   },
+//   // },
+//   // plugins: [
+//   //   react(),
+//   // ],
 // })
 
 
-import { defineConfig } from 'vite'
 
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:3000',
-  //       secure: false,
-  //     },
-  //   },
-  // },
-  // plugins: [
-  //   react(),
-  // ],
+  plugins: [tailwindcss(), flowbiteReact()],
 })
