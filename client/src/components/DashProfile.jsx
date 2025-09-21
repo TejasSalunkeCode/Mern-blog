3040a51da3efe270aabd16a6aa73ee2c91303bb7
TejasSalunkeCode <tejassalunke8788@gmail.com>
2025-09-21 11:35:22 +0530

    Complete sidebar of the dashboard

---
+++ client/src/components/DashProfile.jsx
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+// export default function DashProfile() {
+//   return (
+//     <div>
+//       <h1 className='bg-amber-200'>DashBoardProfile</h1>
+//     </div>
+//   )
+// }
+
+
+// src/components/DashProfile.jsx
+import React from "react";
+
+export default function DashProfile() {
+  return (
+    <div>
+      <h2 className>Profile Tab</h2>
+    </div>
+  );
+}
